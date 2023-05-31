c1560578826de16f74505efceba208dcc5a89e67
Steve Palero <logicallydimp@gmail.com>
2023-05-31 11:07:03 +0800

    chore: added shadcn ui

--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -5,6 +5,9 @@ import loginUser from "./api/loginUser";
 import { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+
 function LoginScreen() {
   const navigate = useNavigate();
   const [setUser] = useStore((state) => [
@@ -38,7 +41,7 @@ function LoginScreen() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center text-black min-h-screen -mt-32 gap-2">
+    <main className="flex flex-col items-center justify-center text-black min-h-screen -mt-32 gap-2 p-14">
       <div className="items-center justify-center font-mono text-sm px-10 pt-8 flex flex-row">
         <img src={logo} style={{ height: 25, width: 75 }} alt="Logo" />
         <div className="left-0 top-0 w-full justify-center text-4xl flex-1 font-bold flex flex-row ml-4">
@@ -57,23 +60,28 @@ function LoginScreen() {
         className="flex flex-col bg-white w-full text-white items-center justify-center max-w-lg gap-4"
         onSubmit={attemptLogin}
       >
-        <input
-          type="text"
-          id="email"
-          className="text-black p-1 rounded-md border px-5 font-normal text-base w-full"
-          placeholder="Email"
-          autoCapitalize="none"
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-          value={form.email}
-        ></input>
-        <input
-          type="password"
-          id="password"
-          className="text-black p-1 rounded-md border px-5 font-normal text-base w-full"
-          placeholder="Password"
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-          value={form.password}
-        ></input>
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="email" className="text-black font-medium text-sm self-start">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            className="text-black p-1 rounded-md border px-5 font-normal text-base w-full mt-1.5"
+            autoCapitalize="none"
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            value={form.email}
+          />
+        </div>
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="password" className="text-black font-medium text-sm self-start">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            className="text-black p-1 rounded-md border px-5 font-normal text-base w-full mt-1.5"
+            autoCapitalize="none"
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            value={form.password}
+          />
+        </div>
         <button
           className="bg-sky-400 py-2 px-10 rounded-md font-medium"
           type="submit"
