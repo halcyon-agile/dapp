1aa099441711dcf58efc84b86d888bd77d5f0e3a
John Steve Palero <jtpalero.halcyondigital@gmail.com>
2023-06-05 12:35:30 +0800

    chore: update timer format

--- src/components/ui/timer.tsx
+++ src/components/ui/timer.tsx
@@ -5,13 +5,18 @@ function Timer(props: { started_at: string }) {
   const [time, setTime] = useState("")
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(moment(props.started_at).toNow(true))
+      const timeDiff = moment.duration(moment().diff(moment(props?.started_at)))
+      const hour = timeDiff.hours()
+      const formatHour = Math.floor(hour)
+      const minute = timeDiff.minutes()
+      const formatMinute = Math.floor(minute)
+      setTime(`${("0"+ formatHour).slice(-2)}:${("0"+ formatMinute).slice(-2)}`)
     }, 1000)
     return () => clearInterval(interval)
   }, [time])
   return (
     <p className="font-medium text-base text-gray-700">
-      {time} ago
+      {time}
     </p>
   )
 }
