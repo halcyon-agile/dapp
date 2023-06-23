99e6f532746da2861d91b3844c7fb2c5fa7adb64
Jerico Pulvera <jerico@halcyondigitalmedia.com>
2023-06-23 12:31:37 +0800

    remove main screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,5 @@
 import LoginScreen from "./LoginScreen";
 
-import MainScreen from "./MainScreen";
 import TakeABreak from "./TakeABreak";
 import SelectAProject from "./SelectAProject";
 import MultipleProjects from "./MultipleProjects";
