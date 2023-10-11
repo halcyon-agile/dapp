939d9f829b64f99d8a6e787e4989490657b61a0b
John Steve Palero <logicallydimp@gmail.com>
2023-10-11 10:14:50 +0800

    chore: fix password input

--- src/screens/Authentication/LoginScreen.tsx
+++ src/screens/Authentication/LoginScreen.tsx
@@ -100,6 +100,10 @@ function LoginScreen() {
             /> */}
             <input
               type={secure ? "password" : "text"}
+              id="password"
+              autoCapitalize="none"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              value={form.password}
               className="flex h-10 w-full text-black p-1 font-normal text-base rounded-md bg-transparent px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
               onFocus={() => setPasswordFocused(true)}
               onBlur={() => setPasswordFocused(false)}
